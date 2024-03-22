d7605623b15d9143971d3605cafb3c80f6ce87e1
NouKinSG <xiaojin@stud.tjut.edu.cn>
2024-03-22 14:29:27 +0800

    longestCommonSubsequence

---
+++ Class3/ten_Sort/Code04_insertionSort/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+// func insertionSort(arr []int) []int {
+
+// }
+func main() {
+	s := "Hello, 世界"
+	for i, j := range s {
+		fmt.Println("索引：", i, "，", "值为：", j)
+	}
+}
