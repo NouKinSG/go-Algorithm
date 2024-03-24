302f43d36fb85d33639360743cd3599c4e6b0508
NouKinSG <xiaojin@stud.tjut.edu.cn>
2024-03-24 19:01:12 +0800

    longestCommonSub for two

---
+++ Class3/Code04_longestCommonSubsequence/mian2.go
@@ -0,0 +1,20 @@
+package main
+
+func longestCommonSubsequence2(text1 string, text2 string) int {
+	m, n := len(text1), len(text2)
+	dp := make([][]int, m+1)
+	for i := range dp {
+		dp[i] = make([]int, n+1)
+	}
+
+	for i := 1; i <= m; i++ {
+		for j := 1; j <= n; j++ {
+			if text1[i-1] == text2[j-1] {
+				dp[i][j] = dp[i-1][j-1]
+			} else {
+				dp[i][j] = max(dp[i][j-1], dp[i-1][j])
+			}
+		}
+	}
+	return dp[m][n]
+}
