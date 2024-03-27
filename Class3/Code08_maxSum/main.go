7c38bfd2fd9dedbbf6c970e7d7c64076436317e5
NouKinSG <xiaojin@stud.tjut.edu.cn>
2024-03-27 10:08:14 +0800

    maxSum

---
+++ Class3/Code08_maxSum/main.go
@@ -0,0 +1,19 @@
+package main
+
+func maxSubArray(nums []int) int {
+	ans := 0
+
+	if len(nums) == 0 {
+		return ans
+	}
+	for i := 1; i < len(nums); i++ {
+		if nums[i]+nums[i-1] > nums[i] {
+			nums[i] += nums[i-1]
+		}
+		if ans < nums[i] {
+			ans = nums[i]
+		}
+	}
+
+	return ans
+}
