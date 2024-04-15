f4bfc44fc34e43b0765600f19ac32f12eda2902a
NouKinSG <xiaojin@stud.tjut.edu.cn>
2024-04-16 00:28:18 +0800

    threeSum

---
+++ Class4/Code05_threeSum/two/main.go
@@ -0,0 +1,38 @@
+package main
+
+import "sort"
+
+func threeSum(nums []int) [][]int {
+	sort.Ints(nums)
+	n := len(nums)
+	ans := [][]int{}
+
+	for i := 0; i < n; i++ {
+		// 跳过重复的
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		target := -nums[i]
+		left, right := i+1, n-1
+		if left < right {
+			sum := nums[left] + nums[right]
+			if sum == target {
+				ans := append(ans, []int{nums[i], nums[left], nums[right]})
+				// 跳过重复元素
+				for left < right && nums[left] == nums[left+1] {
+					left++
+				}
+				for left < right && nums[right] == nums[right-1] {
+					right--
+				}
+				left++
+				right--
+			} else if sum < target {
+				left++
+			} else {
+				right--
+			}
+		}
+	}
+	return ans
+}
