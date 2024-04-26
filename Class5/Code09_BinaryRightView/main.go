abdce8cc3737eea3b20e24fc0e326eb67b7681f8
NouKinSG <xiaojin@stud.tjut.edu.cn>
2024-04-26 11:01:46 +0800

    binaryRightView

---
+++ Class5/Code09_BinaryRightView/main.go
@@ -0,0 +1,35 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// 199. 二叉树的右视图
+func rightSideView(root *TreeNode) []int {
+	if root == nil {
+		return nil
+	}
+	ans := []int{}
+	queue := []*TreeNode{}
+	queue = append(queue, root)
+	for len(queue) > 0 {
+		n := len(queue)
+		for i := 0; i < n; i++ {
+			cur := queue[0]
+			queue = queue[1:]
+			if cur.Left != nil {
+				queue = append(queue, cur.Left)
+			}
+			if cur.Right != nil {
+				queue = append(queue, cur.Right)
+			}
+			if i == n-1 {
+				ans = append(ans, cur.Val)
+			}
+		}
+	}
+
+	return ans
+}
