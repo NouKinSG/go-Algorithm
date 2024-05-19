c4bcb3842afa8899f13a307f18b8acc52ad818e6
NouKinSG <xiaojin@stud.tjut.edu.cn>
2024-05-19 21:38:08 +0800

    megerTwoArray

---
+++ Class7/mergeTwoArray.go
@@ -0,0 +1,25 @@
+package Class7
+
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	sorted := make([]int, 0, m+n)
+	p1, p2 := 0, 0
+	for {
+		if p1 == m {
+			sorted = append(sorted, nums2[p2:]...)
+			break
+		}
+		if p2 == n {
+			sorted = append(sorted, nums1[p1:]...)
+			break
+		}
+
+		if nums1[p1] < nums2[p2] {
+			sorted = append(sorted, nums1[p1])
+			p1++
+		} else {
+			sorted = append(sorted, nums2[p2])
+			p2++
+		}
+	}
+	copy(nums1, sorted)
+}
