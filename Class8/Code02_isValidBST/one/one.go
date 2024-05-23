ff49fd26245d6d6f571fd1c6348168b418f95826
NouKinSG <xiaojin@stud.tjut.edu.cn>
2024-05-23 13:44:51 +0800

    isValidBST

---
+++ Class8/Code02_isValidBST/one/one.go
@@ -0,0 +1,28 @@
+package one
+
+import "math"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isValidBST(root *TreeNode) bool {
+	stack := []*TreeNode{}
+	inorder := math.MinInt64
+	for len(stack) > 0 || root != nil {
+		for root != nil {
+			stack = append(stack, root)
+			root = root.Left
+		}
+		root = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		if root.Val <= inorder {
+			return false
+		}
+		inorder = root.Val
+		root = root.Right
+	}
+	return true
+}
