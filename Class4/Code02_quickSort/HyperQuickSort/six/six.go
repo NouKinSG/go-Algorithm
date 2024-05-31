70300d7ac7cf41beced4df4c117d4bd67d60bdb3
NouKinSG <xiaojin@stud.tjut.edu.cn>
2024-05-31 09:04:19 +0800

    feat: quickSortsix

---
+++ Class4/Code02_quickSort/HyperQuickSort/six/six.go
@@ -0,0 +1,39 @@
+package six
+
+import (
+	"math/rand"
+	"time"
+)
+
+func sortArray(nums []int) []int {
+	rand.Seed(time.Now().Unix())
+	quickSort(nums, 0, len(nums)-1)
+	return nums
+}
+
+func quickSort(nums []int, left, right int) {
+	if left >= right {
+		return
+	}
+	lt, gt := left, right
+	pivotIndex := rand.Intn(right-left+1) + left
+	nums[left], nums[pivotIndex] = nums[pivotIndex], nums[left]
+	pivot := nums[left]
+
+	i := lt + 1
+	for i <= gt {
+		if nums[i] < pivot {
+			nums[i], nums[lt] = nums[lt], nums[i]
+			i++
+			lt++
+		} else if nums[i] > pivot {
+			nums[i], nums[gt] = nums[gt], nums[i]
+			gt--
+		} else {
+			i++
+		}
+	}
+	// 递归两边
+	quickSort(nums, left, lt-1)
+	quickSort(nums, gt+1, right)
+}
