4b08dd711b578312571237452c9510cd05131a97
NouKinSG <xiaojin@stud.tjut.edu.cn>
2024-05-31 09:08:55 +0800

    mergeSort

---
+++ Class8/Code05_mergeSort/one/one.go
@@ -0,0 +1,28 @@
+package one
+
+// 912. 排序数组（归并排序）
+func sortArray(nums []int) []int {
+	if len(nums) <= 1 {
+		return nums
+	}
+	mid := len(nums) / 2
+	left := sortArray(nums[:mid])
+	right := sortArray(nums[mid:])
+	return merge(left, right)
+}
+func merge(left, right []int) []int {
+	ans := make([]int, 0, len(left)+len(right))
+	i, j := 0, 0
+	for i < len(left) && j < len(right) {
+		if left[i] < right[j] {
+			ans = append(ans, left[i])
+			i++
+		} else {
+			ans = append(ans, right[j])
+			j++
+		}
+	}
+	ans = append(ans, left[i:]...)
+	ans = append(ans, right[j:]...)
+	return ans
+}
