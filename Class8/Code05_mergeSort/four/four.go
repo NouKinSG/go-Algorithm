9442349bfc4e4926bb57004c29336507192bda29
NouKinSG <xiaojin@stud.tjut.edu.cn>
2024-06-05 18:50:03 +0800

    mergeSort

--- Class8/Code05_mergeSort/four/four.go
+++ Class8/Code05_mergeSort/four/four.go
@@ -1,16 +1,19 @@
-package two
+package four
 
-// 912. 排序数组（归并排序）
 func sortArray(nums []int) []int {
+	// 递归终止条件
 	if len(nums) <= 1 {
 		return nums
 	}
+	//中间
 	mid := len(nums) / 2
+	// 左右
 	left := sortArray(nums[:mid])
 	right := sortArray(nums[mid:])
 	return merge(left, right)
 }
 func merge(left, right []int) []int {
+	// 定义 答案 ans
 	ans := make([]int, 0, len(left)+len(right))
 	i, j := 0, 0
 	for i < len(left) && j < len(right) {
@@ -22,6 +25,7 @@ func merge(left, right []int) []int {
 			j++
 		}
 	}
+	// 把剩下的元素加入 ans
 	ans = append(ans, left[i:]...)
 	ans = append(ans, right[j:]...)
 	return ans
