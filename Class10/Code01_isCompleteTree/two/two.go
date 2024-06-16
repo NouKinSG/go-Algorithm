e261af1628b4802787fedf74e05599ebcf867f38
NouKinSG <xiaojin@stud.tjut.edu.cn>
2024-06-16 17:02:56 +0800

    isCompleteTree for two

---
+++ Class10/Code01_isCompleteTree/two/two.go
@@ -0,0 +1,36 @@
+package two
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isCompleteTree(root *TreeNode) bool {
+	// 空树也算
+	if root == nil {
+		return true
+	}
+
+	//	使用切片队列
+	queue := []*TreeNode{root}
+	// 标识位
+	readflag := false
+
+	for len(queue) > 0 {
+		//队列
+		cur := queue[0]
+		queue = queue[1:]
+		if cur != nil {
+			if readflag {
+				return false
+			}
+			queue = append(queue, cur.Left)
+			queue = append(queue, cur.Right)
+		} else {
+			// 如果出现了空，那就以后必须为空
+			readflag = true
+		}
+	}
+	return true
+}
