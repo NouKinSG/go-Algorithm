18ea7144d412cd9fc7218d90138c43b45498ad0d
tengzhaojin1 <tengzhaojin1@tal.com>
2024-07-02 06:46:17 +0800

    map stady

--- ACM/base/Class1/deferAndErr/main/main.go
+++ ACM/base/Class1/deferAndErr/main/main.go
@@ -1,13 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("func start")
-	x := 10
-	defer func(x int) {
-		fmt.Println("in defer: ", x)
-	}(x)
-	x = 30
-	fmt.Println("func end: ", x)
+	fmt.Println("start main")
+	fn2()
+	fmt.Println("end main")
+}
+
+func fn2() {
+	fmt.Println("start fn22222")
+	fn()
+	fmt.Println("end fn22222")
+}
+
+func fn() {
+	fmt.Println("start fn")
+	panic("pannic in fn")
+	fmt.Println("end fn")
+}
+
+// 会引发panic的sum
+func sum(x, y *int) int {
+	return *x + *y
 }
