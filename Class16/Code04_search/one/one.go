6ea13986f88db5288850f01299bb015bdfa5366a
NouKinSG <xiaojin@stud.tjut.edu.cn>
2024-08-09 20:59:34 +0800

    serach 81. 搜索旋转排序数组 II

---
+++ Class16/Code04_search/one/one.go
@@ -0,0 +1,35 @@
+package one
+
+func search(nums []int, target int) bool {
+	if len(nums) == 0 {
+		return false
+	}
+	left, right := 0, len(nums)-1
+
+	for left <= right {
+		mid := left + (right-left)/2
+
+		if nums[mid] == target {
+			return true
+		}
+
+		// 处理重复元素
+		if nums[left] == nums[mid] && nums[mid] == nums[right] {
+			left++
+			right--
+		} else if nums[left] <= nums[mid] { // 左半部分是有序的
+			if nums[left] <= target && target < nums[mid] {
+				right = mid - 1
+			} else {
+				left = mid + 1
+			}
+		} else { // 右半部分是有序的
+			if nums[mid] < target && target <= nums[right] {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
+		}
+	}
+	return false
+}
